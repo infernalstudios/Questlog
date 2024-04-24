5ffec7cf01308ce7a64ac22135ab848c3ddb2746
Kārlis Čerņavskis <karliscern@gmail.com>
2024-04-24 23:05:56 +0300

    why did i even do this

--- src/main/java/org/infernalstudios/questlog/network/NetworkHandler.java
+++ src/main/java/org/infernalstudios/questlog/network/NetworkHandler.java
@@ -1,9 +1,7 @@
 package org.infernalstudios.questlog.network;
 
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
@@ -15,12 +13,6 @@ import org.infernalstudios.questlog.network.packet.QuestDefinitionPacket;
 import org.infernalstudios.questlog.network.packet.QuestRemovePacket;
 import org.infernalstudios.questlog.network.packet.QuestRewardCollectPacket;
 import org.infernalstudios.questlog.network.packet.QuestTriggeredPacket;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class NetworkHandler {
   private static final String PROTOCOL_VERSION = "1";
@@ -37,62 +29,47 @@ public class NetworkHandler {
   public static synchronized void register() {
     Questlog.LOGGER.debug("Registering network packets");
 
-    registerPacket(QuestDataPacket.class);
-    registerPacket(QuestDefinitionPacket.class);
-    registerPacket(QuestDefinitionHandledPacket.class);
-    registerPacket(QuestRemovePacket.class);
-    registerPacket(QuestTriggeredPacket.class);
-    registerPacket(QuestCompletedPacket.class);
-    registerPacket(QuestRewardCollectPacket.class);
-  }
+    INSTANCE.messageBuilder(QuestDataPacket.class, PACKET_ID_COUNTER++)
+      .encoder(QuestDataPacket::encode)
+      .decoder(QuestDataPacket::decode)
+      .consumerMainThread(QuestDataPacket::handle)
+      .add();
 
-  @SuppressWarnings("unchecked")
-  private static synchronized <M> void registerPacket(Class<M> packetClass) {
-    BiConsumer<M, FriendlyByteBuf> encode;
-    Function<FriendlyByteBuf, M> decode;
-    BiConsumer<M, Supplier<NetworkEvent.Context>> handle;
-    try {
-      Method encodeMethod = packetClass.getMethod("encode", FriendlyByteBuf.class);
-      if (encodeMethod.getReturnType() != void.class) {
-        throw new IllegalArgumentException("Encode method must return void");
-      }
-      encode = (packet, friendlyByteBuf) -> {
-        try {
-          encodeMethod.invoke(packet, friendlyByteBuf);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-          Questlog.LOGGER.error("Failed to encode " + packetClass.getName(), e);
-          throw new RuntimeException(e);
-        }
-      };
-      Method decodeMethod = packetClass.getMethod("decode", FriendlyByteBuf.class);
-      if (decodeMethod.getReturnType() != packetClass) {
-        throw new IllegalArgumentException("Decode method must return the packet class");
-      }
-      decode = (friendlyByteBuf) -> {
-        try {
-          return (M) decodeMethod.invoke(null, friendlyByteBuf);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-          Questlog.LOGGER.error("Failed to decode " + packetClass.getName(), e);
-          throw new RuntimeException(e);
-        }
-      };
-      Method handleMethod = packetClass.getMethod("handle", packetClass, Supplier.class);
-      if (handleMethod.getReturnType() != void.class) {
-        throw new IllegalArgumentException("Handle method must return void");
-      }
-      handle = (packet, supplier) -> {
-        try {
-          handleMethod.invoke(null, packet, supplier);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-          Questlog.LOGGER.error("Failed to handle " + packetClass.getName(), e);
-          throw new RuntimeException(e);
-        }
-      };
-    } catch (NoSuchMethodException e) {
-      throw new RuntimeException(e);
-    }
+    INSTANCE.messageBuilder(QuestDefinitionPacket.class, PACKET_ID_COUNTER++)
+      .encoder(QuestDefinitionPacket::encode)
+      .decoder(QuestDefinitionPacket::decode)
+      .consumerMainThread(QuestDefinitionPacket::handle)
+      .add();
 
-    INSTANCE.messageBuilder(packetClass, PACKET_ID_COUNTER++).encoder(encode).decoder(decode).consumerMainThread(handle).add();
+    INSTANCE.messageBuilder(QuestDefinitionHandledPacket.class, PACKET_ID_COUNTER++)
+      .encoder(QuestDefinitionHandledPacket::encode)
+      .decoder(QuestDefinitionHandledPacket::decode)
+      .consumerMainThread(QuestDefinitionHandledPacket::handle)
+      .add();
+
+    INSTANCE.messageBuilder(QuestRemovePacket.class, PACKET_ID_COUNTER++)
+      .encoder(QuestRemovePacket::encode)
+      .decoder(QuestRemovePacket::decode)
+      .consumerMainThread(QuestRemovePacket::handle)
+      .add();
+
+    INSTANCE.messageBuilder(QuestTriggeredPacket.class, PACKET_ID_COUNTER++)
+      .encoder(QuestTriggeredPacket::encode)
+      .decoder(QuestTriggeredPacket::decode)
+      .consumerMainThread(QuestTriggeredPacket::handle)
+      .add();
+
+    INSTANCE.messageBuilder(QuestCompletedPacket.class, PACKET_ID_COUNTER++)
+      .encoder(QuestCompletedPacket::encode)
+      .decoder(QuestCompletedPacket::decode)
+      .consumerMainThread(QuestCompletedPacket::handle)
+      .add();
+
+    INSTANCE.messageBuilder(QuestRewardCollectPacket.class, PACKET_ID_COUNTER++)
+      .encoder(QuestRewardCollectPacket::encode)
+      .decoder(QuestRewardCollectPacket::decode)
+      .consumerMainThread(QuestRewardCollectPacket::handle)
+      .add();
   }
 
   public static <M> void sendToPlayer(M message, ServerPlayer player) {
