64189cba98ceb880f37bf2f005a82432096c4a77
Kārlis Čerņavskis <karliscern@gmail.com>
2025-03-19 15:49:57 +0200

    packets on fabric execute on main thread, fixes #12

--- fabric/src/main/java/org/infernalstudios/questlog/networking/QuestlogPacketsFabric.java
+++ fabric/src/main/java/org/infernalstudios/questlog/networking/QuestlogPacketsFabric.java
@@ -3,6 +3,8 @@ package org.infernalstudios.questlog.networking;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.world.entity.player.Player;
+import org.infernalstudios.questlog.mixin.fabric.ServerGamePacketListenerImplAccessor;
+import org.infernalstudios.questlog.mixin.fabric.client.ClientPacketListenerAccessor;
 import org.infernalstudios.questlog.network.IPacketContext;
 import org.infernalstudios.questlog.network.QuestlogPackets;
 
@@ -20,34 +22,38 @@ public class QuestlogPacketsFabric {
     private static <T> void registerS2CPacket(QuestlogPackets.RegisteredPacket<T> registered) {
         ClientPlayNetworking.registerGlobalReceiver(registered.id(), (client, handler, buf, responseSender) -> {
             T packet = registered.decoder().apply(buf);
-            registered.handler().accept(packet, new IPacketContext() {
-                @Override
-                public Player getSender() {
-                    return null;
-                }
+            ((ClientPacketListenerAccessor) handler).getMinecraft().execute(() ->
+                registered.handler().accept(packet, new IPacketContext() {
+                    @Override
+                    public Player getSender() {
+                        return null;
+                    }
 
-                @Override
-                public Direction getDirection() {
-                    return Direction.SERVER_TO_CLIENT;
-                }
-            });
+                    @Override
+                    public Direction getDirection() {
+                        return Direction.SERVER_TO_CLIENT;
+                    }
+                })
+            );
         });
     }
 
     private static <T> void registerC2SPacket(QuestlogPackets.RegisteredPacket<T> registered) {
         ServerPlayNetworking.registerGlobalReceiver(registered.id(), (server, player, handler, buf, responseSender) -> {
             T packet = registered.decoder().apply(buf);
-            registered.handler().accept(packet, new IPacketContext() {
-                @Override
-                public Player getSender() {
-                    return player;
-                }
+            ((ServerGamePacketListenerImplAccessor) handler).getServer().execute(() ->
+                registered.handler().accept(packet, new IPacketContext() {
+                    @Override
+                    public Player getSender() {
+                        return player;
+                    }
 
-                @Override
-                public Direction getDirection() {
-                    return Direction.CLIENT_TO_SERVER;
-                }
-            });
+                    @Override
+                    public Direction getDirection() {
+                        return Direction.CLIENT_TO_SERVER;
+                    }
+                })
+            );
         });
     }
 
